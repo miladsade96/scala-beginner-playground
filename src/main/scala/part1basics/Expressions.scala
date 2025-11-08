b04b05d38c51ad25804b9af3f6b23ba2f81c50f1
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-08 20:52:10 +0330

    Add expressions and evaluations in Expressions.scala

--- src/main/scala/part1basics/Expressions.scala
+++ src/main/scala/part1basics/Expressions.scala
@@ -2,5 +2,24 @@ package tech.miladsadeghi
 package part1basics
 
 object Expressions {
-  
+
+  // Expressions are structures that can be evaluated to a value:
+  val meaningOfLife: Int = 40 + 2
+
+  // Mathematical Expressions: +, -, *, /, bitwise & , | , ^ , << , >> , >>>
+  private val mathExpression = (5 + 3) * 2
+
+
+  // Comparison Expressions: ==, !=, >, >=, <, <=
+  private val isEqual = (5 + 3) == 8
+
+  // Boolean Expressions: &&, ||, !
+  private val isTrue = !isEqual && (meaningOfLife > 40)
+
+  def main(args: Array[String]): Unit = {
+    println(meaningOfLife)
+    println(mathExpression)
+    println(isEqual)
+    println(isTrue)
+  }
 }
