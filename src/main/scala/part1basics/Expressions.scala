e23f493cc8c5e32365d965a10f0f1d5023fca4d3
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-12 23:05:22 +0330

    Add exercises and explanations in Expressions.scala

--- src/main/scala/part1basics/Expressions.scala
+++ src/main/scala/part1basics/Expressions.scala
@@ -27,8 +27,26 @@ object Expressions {
     val y = 20 // local value
     x + y // last expression is the value of the code block
   }
+
   //--------------------------------------------------------------------------------------------------------------------
-  
+  // Exercises:
+  // 1. What is the value of the code block?
+  private val someValue: Boolean = {
+    2 < 3
+  }
+
+  // 2. What is the value of the code block?
+  private val someOtherValue: Int = {
+    if (someValue) 42 else 65
+    50
+  }
+
+  // 3. What is the type of the code?
+  private val yetAnotherValue: Unit = println("Scala")
+  // Explanation: The value of the code block is of type Unit because the last expression is a println statement, which returns Unit.
+  // Unit in Scala is similar to void in other programming languages, indicating the absence of a meaningful value.
+
+
   def main(args: Array[String]): Unit = {
     println("meaningOfLife:   " + meaningOfLife)
     println("mathExpression:   " + mathExpression)
@@ -36,5 +54,9 @@ object Expressions {
     println("isTrue:   " + isTrue)
     println("anIfExpression:   " + anIfExpression)
     println("aCodeBlock:   " + aCodeBlock)
+    // Exercises:
+    println("someValue:   " + someValue)
+    println("someOtherValue:   " + someOtherValue)
+    println("yetAnotherValue:   " + yetAnotherValue)
   }
 }
