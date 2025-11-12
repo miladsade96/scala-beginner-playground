adf1917b92c08ec7e3301f308fc89170a59a8ad2
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-12 22:48:36 +0330

    Enhance Expressions.scala with additional expression examples and comments

--- src/main/scala/part1basics/Expressions.scala
+++ src/main/scala/part1basics/Expressions.scala
@@ -5,21 +5,28 @@ object Expressions {
 
   // Expressions are structures that can be evaluated to a value:
   val meaningOfLife: Int = 40 + 2
-
+  //--------------------------------------------------------------------------------------------------------------------
   // Mathematical Expressions: +, -, *, /, bitwise & , | , ^ , << , >> , >>>
   private val mathExpression = (5 + 3) * 2
-
-
+  //--------------------------------------------------------------------------------------------------------------------
   // Comparison Expressions: ==, !=, >, >=, <, <=
   private val isEqual = (5 + 3) == 8
-
+  //--------------------------------------------------------------------------------------------------------------------
   // Boolean Expressions: &&, ||, !
   private val isTrue = !isEqual && (meaningOfLife > 40)
+  //--------------------------------------------------------------------------------------------------------------------
+  // Instruction vs. Expression
+  // Instructions are commands that perform actions but do not return values.
+  // Expressions return values and can be composed together.
+  private val aCondition = true
+  private val anIfExpression: Int = if (aCondition) 5 else 3
+  //--------------------------------------------------------------------------------------------------------------------
 
   def main(args: Array[String]): Unit = {
     println(meaningOfLife)
     println(mathExpression)
     println(isEqual)
     println(isTrue)
+    println(anIfExpression)
   }
 }
