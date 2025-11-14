0ab237c4554a06b10578f1c63e50d9b457d029a3
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-14 15:16:06 +0330

    Add function definition and invocation example in Functions.scala

--- src/main/scala/part1basics/Functions.scala
+++ src/main/scala/part1basics/Functions.scala
@@ -2,7 +2,17 @@ package tech.miladsadeghi
 package part1basics
 
 object Functions {
+
+  // A function is a reusable block of code that takes some inputs and produces an output.
+  // In Scala, functions are first-class citizens, meaning they can be assigned to variables,
+  // passed as arguments to other functions, and returned from other functions.
+  private def aFunction(a: String, b: Int): String = {
+    a + " " + b // one expression
+  }
+
+  // Function invocation
+  private val aFunctionInvocation: String = aFunction("Hello", 3) // "Hello 3"
   def main(args: Array[String]): Unit = {
-    
+    println("aFunctionInvocation: " + aFunctionInvocation)
   }
 }
